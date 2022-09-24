4a11b8be8640e40875626a4968cd0f1852e86c09
Valentin Vlad <55762636+vvalentinv@users.noreply.github.com>
2022-09-24 14:04:08 -0400

    test heroku backend

--- login.js
+++ login.js
@@ -2,39 +2,40 @@ let usernameInput = document.getElementById('username');
 let passwordInput = document.getElementById('password');
 let loginSubmitButton = document.getElementById('login-submit-btn');
 let loginErrorMessageDiv = document.getElementById('login-error-message')
+let url = "http://172.16.19.218:41626"
 
 loginSubmitButton.addEventListener('click', async (e) => {
   e.preventDefault();
-  // try {
-  let res = await fetch('http://127.0.0.1:8080/login', {
-    'credentials': 'include',
-    'method': 'POST',
-    'headers': {
-      'Content-Type': 'application/json'
-    },
-    'body': JSON.stringify({
-      "username": usernameInput.value,
-      "password": passwordInput.value
+  try {
+    let res = await fetch(`${url}/login`, {
+      'credentials': 'include',
+      'method': 'POST',
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify({
+        "username": usernameInput.value,
+        "password": passwordInput.value
+      })
     })
-  })
-  if (res.status == 200) {
-    window.location.href = '/reimb-view.html';
-  } else if (res.status == 401) {
-    errorElement.innerHTML = "";
-    let data = await res.json();
-    let errorElement = document.createElement('p');
-    errorElement.innerHTML = data.message;
-    errorElement.style.color = 'red';
-    errorElement.style.fontWeight = 'bold';
-    loginErrorMessageDiv.appendChild(errorElement);
+    if (res.status == 200) {
+      window.location.href = '/reimb-view.html';
+    } else if (res.status == 401) {
+      errorElement.innerHTML = "";
+      let data = await res.json();
+      let errorElement = document.createElement('p');
+      errorElement.innerHTML = data.message;
+      errorElement.style.color = 'red';
+      errorElement.style.fontWeight = 'bold';
+      loginErrorMessageDiv.appendChild(errorElement);
+    }
+  } catch (err) {
+    if (err.message == "Failed to fetch") {
+      let errorElement = document.createElement('p');
+      errorElement.innerHTML = err.message;
+      errorElement.style.color = 'red';
+      errorElement.style.fontWeight = 'bold';
+      loginErrorMessageDiv.appendChild(errorElement);
+    }
   }
-  // } catch (err) {
-  //   if (err.message == "Failed to fetch") {
-  //     let errorElement = document.createElement('p');
-  //     errorElement.innerHTML = "Server issue: contact IT Admin";
-  //     errorElement.style.color = 'red';
-  //     errorElement.style.fontWeight = 'bold';
-  //     loginErrorMessageDiv.appendChild(errorElement);
-  //   }
-  // }
 });
